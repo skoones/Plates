a49ca6ef63bcda5dea70ee625d1a6d5a93d8f942
Julia Skoneczna <juliaskoneczna@gmail.com>
2021-01-10 20:21:04 +0100

    Removed default vuetify setup page

---
+++ frontend/src/views/WeeklyPlan.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="weeklyPlan">
+    <h1>Hi this is weekly plan</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WeeklyPlan"
+}
+</script>
+
+<style scoped>
+
+</style>
