507aa48032d057e2601cd80c7a281abd52c5cc31
Julia Skoneczna <juliaskoneczna@gmail.com>
2021-01-12 22:29:59 +0100

    Fixed grid system problems in weekly plan

--- frontend/src/components/DayOfWeek.vue
+++ frontend/src/components/DayOfWeek.vue
@@ -1,17 +1,15 @@
 <template>
   <div>
-    <v-container>
-      <v-layout column>
-        <v-flex flat height="100%" tile width="100%">
-          <plan-cell :day-of-week="dayName" :is-meal="false"></plan-cell>
-          <plan-cell :is-meal="true" :meal-type="'breakfast'"></plan-cell>
-          <plan-cell :is-meal="true" :meal-type="'snack'"></plan-cell>
-          <plan-cell :is-meal="true" :meal-type="'lunch'"></plan-cell>
-          <plan-cell :is-meal="true" :meal-type="'snack'"></plan-cell>
-          <plan-cell :is-meal="true" :meal-type="'dinner'"></plan-cell>
-        </v-flex>
-      </v-layout>
-    </v-container>
+    <v-layout column>
+      <v-flex flat tile>
+        <plan-cell :day-of-week="dayName" :is-meal="false"></plan-cell>
+        <plan-cell :is-meal="true" :meal-type="'breakfast'"></plan-cell>
+        <plan-cell :is-meal="true" :meal-type="'snack'"></plan-cell>
+        <plan-cell :is-meal="true" :meal-type="'lunch'"></plan-cell>
+        <plan-cell :is-meal="true" :meal-type="'snack'"></plan-cell>
+        <plan-cell :is-meal="true" :meal-type="'dinner'"></plan-cell>
+      </v-flex>
+    </v-layout>
   </div>
 </template>
 
--- frontend/src/components/WeeklyPlan.vue
+++ frontend/src/components/WeeklyPlan.vue
@@ -2,37 +2,11 @@
   <div>
     <v-container fluid>
       <!-- TODO - w jaki sposób reprezentować posiłki? form, button, nazwa posiłku + button?-->
-      <!--      <v-layout justify-center row>-->
-      <!--        <v-flex :color="'grey darken-3'" class="d-flex flex-row mt-6" flat tile width="100%">-->
-      <!--          <v-card v-for="day in daysOfWeek" :key="day" class="pt-3 pb-3" elevation="3"-->
-      <!--                  outlined tile width="100%">-->
-      <!--            <v-card-title class="text-h5 justify-center grey&#45;&#45;text text&#45;&#45;darken-4">{{ day }}</v-card-title>-->
-      <!--          </v-card>-->
-      <!--        </v-flex>-->
-      <!--      </v-layout>-->
       <v-layout justify-center row>
-        <v-flex>
-          <day-of-week v-for="day in daysOfWeek" :key="day" :day-name="day"></day-of-week>
+        <v-flex v-for="day in daysOfWeek" :key="day">
+          <day-of-week :day-name="day"></day-of-week>
         </v-flex>
       </v-layout>
-
-
-      <!--      <v-layout row>-->
-      <!--        <plan-cell v-for="n in 7" :key="n" :meal-type="'breakfast'" :is-meal="true"></plan-cell>-->
-      <!--      </v-layout>-->
-      <!--      <v-layout row>-->
-      <!--        <plan-cell v-for="n in 7" :key="n" :meal-type="'snack'" :is-meal="true"></plan-cell>-->
-      <!--      </v-layout>-->
-      <!--      <v-layout row>-->
-      <!--        <plan-cell v-for="n in 7" :key="n" :meal-type="'lunch'" :is-meal="true"></plan-cell>-->
-      <!--      </v-layout>-->
-      <!--      <v-layout row>-->
-      <!--        <plan-cell v-for="n in 7" :key="n" :meal-type="'snack'" :is-meal="true"></plan-cell>-->
-      <!--      </v-layout>-->
-      <!--      <v-layout row>-->
-      <!--        <plan-cell v-for="n in 7" :key="n" :meal-type="'dinner'" :is-meal="true"></plan-cell>-->
-      <!--      </v-layout>-->
-
     </v-container>
   </div>
 </template>
