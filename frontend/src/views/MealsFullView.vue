e07885bb618562f13c216ebfb8a28db6d2874595
Julia Skoneczna <juliaskoneczna@gmail.com>
2021-01-14 00:55:28 +0100

    Changed meals full view

--- frontend/src/views/MealsFullView.vue
+++ frontend/src/views/MealsFullView.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
-    <v-container class="ml-5" fluid>
-      <v-row>
-        <v-col cols="1">
-          <v-btn :to="'/week'" class="primary" x-large>Back</v-btn>
-        </v-col>
-        <v-col cols="11">
-          <v-toolbar class="primary">
-            <v-toolbar-title>All meals</v-toolbar-title>
-          </v-toolbar>
-        </v-col>
-      </v-row>
+    <v-container class="mx-5" fluid>
+      <v-card class="d-flex flex-row" flat>
+        <v-btn :to="'/week'" class="primary mt-auto mb-auto mx-5" left x-large>
+          Back
+        </v-btn>
+        <v-toolbar class="primary">
+          <v-toolbar-title>
+            All meals
+          </v-toolbar-title>
+        </v-toolbar>
+      </v-card>
     </v-container>
   </div>
 </template>
