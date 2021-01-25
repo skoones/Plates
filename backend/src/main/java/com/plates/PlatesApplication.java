037fc9e3d65d21985a81c16b4d6ec6c1ba2d3101
Julia Skoneczna <juliaskoneczna@gmail.com>
2021-01-25 04:12:25 +0100

    Added minor comments to the documentation.

--- backend/src/main/java/com/plates/PlatesApplication.java
+++ backend/src/main/java/com/plates/PlatesApplication.java
@@ -3,6 +3,9 @@ package com.plates;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Starting point for Spring Boot application.
+ */
 @SpringBootApplication
 public class PlatesApplication {
 
--- backend/src/test/java/com/plates/PlatesApplicationTests.java
+++ backend/src/test/java/com/plates/PlatesApplicationTests.java
@@ -3,6 +3,9 @@ package com.plates;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * Starting point for Spring Boot application tests.
+ */
 @SpringBootTest
 class PlatesApplicationTests {
 
